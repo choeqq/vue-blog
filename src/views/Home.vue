f6872a6a11778f0286de3332244f1ea9a089c932
choeqq <k.chola@op.pl>
2022-03-28 00:00:48 +0200

    blog post done

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,41 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <BlogPost :post="welcomeScreen" />
+    <BlogPost
+      v-for="(post, index) in sampleBlogPost"
+      :key="index"
+      :post="post"
+    />
+  </div>
 </template>
 
 <script>
+import BlogPost from "../components/BlogPost.vue";
 export default {
   name: "Home",
-  components: {},
+  components: { BlogPost },
+  data() {
+    return {
+      welcomeScreen: {
+        title: "Welcome!",
+        blogPost:
+          "Weekly blog articles with all things programming including HTML, CSS, JavaScript and more. Register today to never miss a post!",
+        welcomeScreen: true,
+        photo: "coding",
+      },
+      sampleBlogPost: [
+        {
+          title: "this is a filter title",
+          blogHTML: "this is a filter blog post title",
+          blogCoverPhoto: "beautiful-stories",
+        },
+        {
+          title: "this is a filter title",
+          blogHTML: "this is a filter blog post title",
+          blogCoverPhoto: "designed-for-everyone",
+        },
+      ],
+    };
+  },
 };
 </script>
