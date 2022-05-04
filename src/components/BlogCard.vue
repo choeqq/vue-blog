8be8522879f1325c035e004f842c164e3df10411
choeqq <k.chola@op.pl>
2022-05-04 22:05:13 +0200

    edit, delete post clicks added

--- src/components/BlogCard.vue
+++ src/components/BlogCard.vue
@@ -1,21 +1,26 @@
 <template>
   <div class="blog-card">
     <div v-show="editPost" class="icons">
-      <div class="icon">
+      <div @click="editBlog" class="icon">
         <Edit class="edit" />
       </div>
-      <div class="icon">
+      <div @click="deletePost" class="icon">
         <Delete class="delete" />
       </div>
     </div>
-    <img
-      :src="require(`../assets/blogCards/${post.blogCoverPhoto}.jpg`)"
-      alt=""
-    />
+    <img :src="post.blogCoverPhoto" alt="" />
     <div class="info">
       <h4>{{ post.blogTitle }}</h4>
-      <h6>Poster on: {{ post.blogDate }}</h6>
-      <router-link class="link" to="#">
+      <h6>
+        Posted on:
+        {{
+          new Date(post.blogDate).toLocaleString("en-us", { dateStyle: "long" })
+        }}
+      </h6>
+      <router-link
+        class="link"
+        :to="{ name: 'ViewBlog', params: { blogid: this.post.blogID } }"
+      >
         View The Post <Arrow class="arrow" />
       </router-link>
     </div>
@@ -33,6 +38,17 @@ export default {
     Arrow,
     Edit,
     Delete,
+  },
+  methods: {
+    deletePost() {
+      this.$store.dispatch("deletePost", this.post.blogID);
+    },
+    editBlog() {
+      this.$router.push({
+        name: "EditBlog",
+        params: { blogid: this.post.blogID },
+      });
+    },
   },
   computed: {
     editPost() {
@@ -52,17 +68,20 @@ export default {
   background-color: #fff;
   min-height: 420px;
   transition: 0.5s ease all;
+
   &:hover {
     transform: rotateZ(-1deg) scale(1.01);
     box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
       0 2px 4px -1px rgba(0, 0, 0, 0.06);
   }
+
   .icons {
     display: flex;
     position: absolute;
     top: 10px;
     right: 10px;
     z-index: 99;
+
     .icon {
       display: flex;
       justify-content: center;
@@ -72,8 +91,10 @@ export default {
       border-radius: 50%;
       background-color: #fff;
       transition: 0.5s ease all;
+
       &:hover {
         background-color: #303030;
+
         .edit,
         .delete {
           path {
@@ -81,9 +102,11 @@ export default {
           }
         }
       }
+
       &:nth-child(1) {
         margin-right: 8px;
       }
+
       .edit,
       .delete {
         pointer-events: none;
@@ -92,6 +115,7 @@ export default {
       }
     }
   }
+
   img {
     display: block;
     border-radius: 8px 8px 0 0;
@@ -100,6 +124,7 @@ export default {
     min-height: 200px;
     object-fit: cover;
   }
+
   .info {
     display: flex;
     flex-direction: column;
